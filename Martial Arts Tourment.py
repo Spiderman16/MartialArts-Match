b99208564585c4d87b0b7632c2d0270d70f773d6
Spiderman16 <90346839+Spiderman16@users.noreply.github.com>
2023-05-19 23:41:12 -0700

    Add files via upload

---
+++ Martial Arts Tourment.py
@@ -0,0 +1,24 @@
+# Prints out a blank line. 
+print("\n\n")
+# Prints the title of this program
+print("Welcome to the Ultimate MartialArts Tournament")
+# Prints out a blank line. 
+print("\n\n")
+# This program will be a varabile being put in for player1
+# by just entering their stage name. 
+user1_name = input("In the right corner is")
+# Prints out a blank line.
+print("\n")
+# This program will be a varabile being put in for player2
+# by just entering their stage name. 
+user2_name = input("In the left corner is")
+# Prints out a blank line. 
+print("\n\n")
+# Print what user1 is saying to user2 
+# It's a string variable kind of program
+print("Bring it on" + user2_name + " I would like to end this match really quick so I can get to my date")
+# Prints out a blank line. 
+print("\n\n")
+# Print what user2 is saying to user1 
+# It's a string variable kind of program
+print("The only date that will be on time for is your doom" + user1_name)
